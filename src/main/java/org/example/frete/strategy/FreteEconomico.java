1949a518d5250140af7cd68976c9716fa1113fc3
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-09 01:24:06 +0200

    freteeconomico

--- src/main/java/org/example/frete/strategy/FreteEconomico.java
+++ src/main/java/org/example/frete/strategy/FreteEconomico.java
@@ -1,4 +1,12 @@
 package org.example.frete.strategy;
 
-public class FreteEconomico {
+public class FreteEconomico implements CalculadoraFrete {
+    public double calcular(double peso) {
+        if (peso <= 0)
+            throw new IllegalArgumentException("Peso deve ser maior que zero");
+        return peso * 1.1 - 5;
+    }
+    public String getTipo() {
+        return "ECO";
+    }
 }
