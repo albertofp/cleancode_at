94ab4a9bff2b7beb297e794dd99418069f001f06
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-09 01:23:45 +0200

    fretepadraoo

--- src/main/java/org/example/frete/strategy/FretePadrao.java
+++ src/main/java/org/example/frete/strategy/FretePadrao.java
@@ -1,4 +1,12 @@
 package org.example.frete.strategy;
 
-public class FretePadrao {
+public class FretePadrao implements CalculadoraFrete {
+    public double calcular(double peso) {
+        if (peso <= 0)
+            throw new IllegalArgumentException("Peso deve ser maior que zero");
+        return peso * 1.2;
+    }
+    public String getTipo() {
+        return "PAD";
+    }
 }
