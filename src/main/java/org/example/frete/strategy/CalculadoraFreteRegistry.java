7b58b1b0a1b257bec0b5b6557531890d0905cd73
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-09 01:24:40 +0200

    freteregistry

--- src/main/java/org/example/frete/strategy/CalculadoraFreteRegistry.java
+++ src/main/java/org/example/frete/strategy/CalculadoraFreteRegistry.java
@@ -1,4 +1,19 @@
 package org.example.frete.strategy;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class CalculadoraFreteRegistry {
+    private static final Map<String, CalculadoraFrete> REGISTRO = new HashMap<>();
+
+    public static void registrar(CalculadoraFrete calculadora) {
+        REGISTRO.put(calculadora.getTipo(), calculadora);
+    }
+
+    public static CalculadoraFrete obter(String tipo) {
+        CalculadoraFrete calc = REGISTRO.get(tipo);
+        if (calc == null)
+            throw new RuntimeException("Tipo de frete inválido: " + tipo);
+        return calc;
+    }
 }
