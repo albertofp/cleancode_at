3cf52281a6eaaf550ee609735a76f0455bef30ea
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-09 01:28:07 +0200

    resumopedidogenerator

--- src/main/java/org/example/frete/strategy/ResumoPedidoGenerator.java
+++ src/main/java/org/example/frete/strategy/ResumoPedidoGenerator.java
@@ -1,4 +1,11 @@
 package org.example.frete.strategy;
 
-public class ResumoPedidoGenerator {
+import org.example.frete.domain.Entrega;
+
+public class ResumoPedidoGenerator implements EtiquetaGenerator {
+    public String gerar(Entrega entrega, double valorFrete) {
+        return "Pedido para " + entrega.getDestinatario() +
+               " com frete tipo " + entrega.getTipoFrete() +
+               " no valor de R$" + valorFrete;
+    }
 }
