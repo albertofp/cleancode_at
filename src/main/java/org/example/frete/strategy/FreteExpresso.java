137dff5042925636054759fedba52e64f73965e2
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-09 01:23:22 +0200

    freteexpresso

--- src/main/java/org/example/frete/strategy/FreteExpresso.java
+++ src/main/java/org/example/frete/strategy/FreteExpresso.java
@@ -1,4 +1,12 @@
 package org.example.frete.strategy;
 
-public class FreteExpresso {
+public class FreteExpresso implements CalculadoraFrete {
+    public double calcular(double peso) {
+        if (peso <= 0)
+            throw new IllegalArgumentException("Peso deve ser maior que zero");
+        return peso * 1.5 + 10;
+    }
+    public String getTipo() {
+        return "EXP";
+    }
 }
