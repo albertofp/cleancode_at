964db9e17dce8a52e5e6d3f8d4259d7f47dec7fe
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-09 01:29:13 +0200

    freteservice

--- src/main/java/org/example/frete/service/FreteService.java
+++ src/main/java/org/example/frete/service/FreteService.java
@@ -1,4 +1,22 @@
 package org.example.frete.service;
 
+import org.example.frete.domain.Entrega;
+import org.example.frete.strategy.CalculadoraFrete;
+import org.example.frete.strategy.CalculadoraFreteRegistry;
+import org.example.frete.strategy.PromocaoFrete;
+
 public class FreteService {
+    private final PromocaoFrete promocao;
+
+    public FreteService(PromocaoFrete promocao) {
+        if (promocao == null)
+            throw new IllegalArgumentException("Promoção nula");
+        this.promocao = promocao;
+    }
+
+    public double calcularFrete(Entrega entrega) {
+        CalculadoraFrete calc = CalculadoraFreteRegistry.obter(entrega.getTipoFrete());
+        double valor = calc.calcular(entrega.getPeso());
+        return promocao.aplicar(entrega, valor);
+    }
 }
