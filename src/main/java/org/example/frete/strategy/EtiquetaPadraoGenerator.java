2087b8539cb8b1010f2ab70c30ff3ff69f57ffa7
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-09 01:27:32 +0200

    etiquetapadraogenerator

--- src/main/java/org/example/frete/strategy/EtiquetaPadraoGenerator.java
+++ src/main/java/org/example/frete/strategy/EtiquetaPadraoGenerator.java
@@ -1,4 +1,11 @@
 package org.example.frete.strategy;
 
-public class EtiquetaPadraoGenerator {
+import org.example.frete.domain.Entrega;
+
+public class EtiquetaPadraoGenerator implements EtiquetaGenerator {
+    public String gerar(Entrega entrega, double valorFrete) {
+        return "Destinatário: " + entrega.getDestinatario() +
+               "\nEndereço: " + entrega.getEndereco() +
+               "\nValor do Frete: R$" + valorFrete;
+    }
 }
