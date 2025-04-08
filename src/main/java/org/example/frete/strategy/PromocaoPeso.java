ea9570408e683a56f9bfa42d8b37ccfe56c9bafe
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-09 01:26:12 +0200

    promocaopeso

--- src/main/java/org/example/frete/strategy/PromocaoPeso.java
+++ src/main/java/org/example/frete/strategy/PromocaoPeso.java
@@ -1,4 +1,11 @@
 package org.example.frete.strategy;
 
-public class PromocaoPeso {
+import org.example.frete.domain.Entrega;
+
+public class PromocaoPeso implements PromocaoFrete {
+    public double aplicar(Entrega entrega, double valorFrete) {
+        if (entrega.getPeso() > 10)
+            return valorFrete - 1;
+        return valorFrete;
+    }
 }
